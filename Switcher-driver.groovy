a89fdf215b9c6c30239066c437f299970e7835db
david-avrahami <david.avrami@gmail.com>
2022-01-11 14:54:33 +0200

    Updated release notes and guide

--- Switcher-driver.groovy
+++ Switcher-driver.groovy
@@ -3,8 +3,12 @@
  *  No license here
  *
  *  How to:
- *  
+ *  You will need to enter the IP and Device ID
  *
+ *  The Device ID can be extracted according to the script: https://github.com/NightRang3r/Switcher-V2-Python
+ *  ******** Big Thanks to the developers Aviad Golan and Shai Rod that created the scripts and made it possible ********
+ *  
+ * 
  *  Changes:
  *  1.0.0   -   Starting project
  *  1.1.0   -   First release
